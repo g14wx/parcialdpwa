58145efcfb8ceed2e5afcfc4ae3899cf23b6c08f
robertofunes98 <robertofunes98@gmail.com>
2021-06-11 19:07:50 -0600

    last changes by funes

--- parcialdpwa/Entities/SelledDisk.cs
+++ parcialdpwa/Entities/SelledDisk.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace parcialdpwa.Entities
 {
-    public class SelledDisk { public int ID; public String Titulo; }
+    [NotMapped]
+    public class SelledDisk {
+        
+        public int ID; public String Titulo; }
 }
